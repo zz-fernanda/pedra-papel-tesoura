00fa6bd667a80de3d60b0d2a317b64022b156ba0
Faveero <159203076+Faveero@users.noreply.github.com>
2024-10-08 16:14:59 -0300

    ewf

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
