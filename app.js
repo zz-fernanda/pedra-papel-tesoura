ec32c425a00ef665d178d1bd0beb9ed211190d24
Faveero <159203076+Faveero@users.noreply.github.com>
2024-10-11 08:16:09 -0300

    few

--- app.js
+++ app.js
@@ -29,10 +29,22 @@ function win(userChoice, computerChoice){
     result_p.innerHTML = `${convertToWord(userChoice)}(smallUserWord) beats ${convertToWord(computerChoice)}(smallCompWord). You win!`;
 }
 
-function lose(){}
+function lose(userChoice, computerChoice){
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallCompWord = "comp".fontsize(3).sub();
+    result_p.innerHTML = `${convertToWord(userChoice)}(smallUserWord) loses to ${convertToWord(computerChoice)}(smallCompWord). You lost...`;
+}
 
 function draw(){
-    console.log("EMPATEEEE")
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallCompWord = "comp".fontsize(3).sub();
+    result_p.innerHTML = `${convertToWord(userChoice)}(smallUserWord) beats ${convertToWord(computerChoice)}(smallCompWord). You win!`;
 }
 
 function game(userChoice){
