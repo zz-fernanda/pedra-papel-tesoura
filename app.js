7420c9f1c420ad3514267da779034b63b5a6cc12
Faveero <159203076+Faveero@users.noreply.github.com>
2024-10-11 09:02:32 -0300

    efw

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 let userScore = 0;
 let computerScore = 0;
-const userScore_span = document.getElementById(".user-score");
-const computerScore_span = document.getElementById(".computer-score");
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
 const result_p = document.querySelector(".result > p");
-const rock_div = document.getElementById(".r");
-const paper_div = document.getElementById(".p");
-const scissors_div = document.getElementById(".s");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
 
 function getComputerChoice(){
     const choices = ['r', 'p', 's'];
